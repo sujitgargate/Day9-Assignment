587c8de6b97ad8d490b6c4926abc4b2abf69f27a
sujit gargate <sujitgargate5893@gmail.com>
2020-07-05 11:30:58 +0530

    Employee wage calculation using function added

---
+++ empWageUsingFunction.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=10;
+empRatePerHour=30;
+totalEmpHrs=0;
+
+numberOfWorkingDays=20;
+totalWorkingDays=0;
+
+function getWorkingHours()
+{
+	#local $empCheck=$1
+	case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+echo $empHours
+
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && 
+				$totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%2))
+	empHours="$( getWorkingHours $empCheck )"
+	totalEmpHrs=$(($totalEmpHrs + $empHours))
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
